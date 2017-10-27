2af522033f44cda964d9dcc2862a80ceed35e5de
collinadams <collinadams04@gmail.com>
2017-10-27 15:31:38 -0700

    exercise 3 dealer

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,54 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+type Card = [Suit, CardNumber];
 
+export class Dealer {
+  cards: Card[] = [];
+  constructor() {
+    this.cards = this.generateDeck();
+    shuffleArray(this.cards);
+  };
+  generateDeck(): Card[] {
+    for (let s = 0; s < Object.keys(Suit).length/2; s++) {
+      for (let n = 0; n < Object.keys(CardNumber).length/2; n++) {
+        this.cards.push([s, n]);
+      }
+    }
+    return this.cards;
+  }
+  readCard(c: Card): string {
+    return `${CardNumber[c[1]]} of ${Suit[c[0]]}`;
+  };
+  getLength(): number {
+    return this.cards.length;
+  };
+  dealHand(numCards: number): Card[] {
+    if (numCards < 0) throw new Error('Hand me YOUR cards');
+    if (numCards > this.cards.length) throw new Error('I do not know what you want');
+    return this.cards.splice(0, numCards);
+  };
+}
+
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+}
+
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades,
 }
