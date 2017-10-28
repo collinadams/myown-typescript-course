4d070f025e0cf532272862df8c574d6538b76317
collinadams <collinadams04@gmail.com>
2017-10-27 17:17:30 -0700

    exercise 5 stack

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,31 @@ interface IStack<T> {
   print(): void;
 }
 
-export class Stack<T> {
-
+export class Stack<T> implements IStack<T> {
+  private layers: T[] = [];
+  push(item: T): IStack<T>;
+  push(items: T[]): IStack<T>;
+  push(maybeItem: T | T[]): IStack<T> {
+    if (maybeItem instanceof Array) {
+    // if (Array.isArray(maybeItem)) {
+      this.layers.push(...maybeItem)
+      // this.layers = this.layers.concat(maybeItem);
+    } else {
+      this.layers.push(maybeItem);
+    }
+    return this;
+  };
+  pop(): T | undefined {
+    if (this.layers.length) {
+      return this.layers.pop();
+    } else {
+      return;
+    }
+  };
+  length(): number {
+    return this.layers.length;
+  };
+  print(): void {
+    console.log(this.layers.join(', '));
+  };
 }
