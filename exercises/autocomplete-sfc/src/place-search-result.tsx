b90fb293c3f736e1e03bd8598c3d28d9b9b75c0c
collinadams <collinadams04@gmail.com>
2017-10-30 13:11:49 -0700

    Exercise 7 place search result 1

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -1,6 +1,47 @@
 import * as React from 'react';
-export const PlaceSearchResult: React.SFC<any> = () => {
+import { PlaceDetails } from './utils/places';
+
+interface PlaceSearchResultProps {
+  result: PlaceDetails
+}
+
+export const PlaceSearchResult: React.SFC<PlaceDetails> = (placeDetail) => {
+  let {
+    id,
+    rating,
+    icon,
+    name,
+    url,
+    vicinity,
+    website,
+  } = placeDetail;
+  let websiteWidget: JSX.Element | null = null;
+  if (website) {
+    websiteWidget = (
+      <span>
+        -
+        <a href={website} target="_blank">
+          {website}
+        </a>
+      </span>
+    )
+  }
   return (
-    <div></div>
+    <li className="search-result">
+      <img
+        className="icon"
+        src={icon}
+      />
+      <h3>
+        {name}
+      </h3>
+      <p>
+        <a href={url} target="_blank">
+          {vicinity}
+        </a>
+
+        {websiteWidget}
+      </p>
+    </li>
   );
 };
